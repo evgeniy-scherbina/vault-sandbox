6779b0d64c7f0d5c07662bf3f62cdf73de91e2b3
evgeniy.scherbina <evgeniy.scherbina@bitfury.com>
2019-12-15 19:11:29 +0200

    Updeted deploy.tf file

--- deploy/terraform/deploy.tf
+++ deploy/terraform/deploy.tf
@@ -40,4 +40,41 @@ resource "aws_instance" "example" {
   instance_type   = "t2.micro"
   key_name        = "ssh-login-key"
   security_groups = ["allow-ssh-login"]
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      host        = aws_instance.example.public_ip
+      agent       = "false"
+      private_key = "${file("/Users/evgeniyscherbina/.ssh/id_rsa")}"
+    }
+
+    inline = [
+      # Install Golang
+      "wget https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz",
+      "tar -xzf go1.12.7.linux-amd64.tar.gz",
+      "sudo mv go /usr/local",
+      "echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile",
+      "echo 'export GOPATH=$HOME/go' >> ~/.bash_profile",
+      "echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile",
+      "source ~/.bash_profile",
+
+      # Clone and compile project
+      "sudo yum install -y git",
+      "git clone https://github.com/evgeniy-scherbina/vault-sandbox.git /home/ec2-user/go/src/github.com/evgeniy-scherbina/vault-sandbox",
+      "go get -u github.com/golang/dep/cmd/dep",
+      // "cd /home/ec2-user/go/src/github.com/evgeniy-scherbina/calc && dep ensure",
+
+      # Install docker
+      "sudo yum install -y docker",
+
+      "curl -L https://releases.hashicorp.com/nomad/0.9.3/nomad_0.9.3_linux_amd64.zip > nomad.zip",
+      "sudo unzip nomad.zip -d /usr/bin",
+
+      "sudo nomad agent -dev &",
+      "sleep 5",
+      "cd deploy/nomad && sudo nomad job run calc.nomad",
+    ]
+  }
 }
